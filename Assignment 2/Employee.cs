41018c689409dc6f3c13ba8d15668c7135225b4a
Prerna Sadegaonkar <prerna.sadegaonkar@capgemini.com>
2022-06-24 19:27:40 +0530

    assignment2

---
+++ Assignment 2/Employee.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+    public class Employee
+    {
+        public int EmpNo { get; set; }
+        public string EmployeeName { get; set; }
+        public double Salary { get; set; }
+        public double HRA { get; set; }
+        public double TA { get; set; }
+        public double DA { get; set; }
+        public double PF { get; set; }
+        public double TDS { get; set; }
+        public double NetSalary { get; set; }
+        public double GrossSalary { get; set; }
+
+        public double CalculateGross()
+        {
+            if (Salary < 5000)
+            {
+                HRA = 0.1 * Salary;
+                TA = 0.05 * Salary;
+                DA = 0.15 * Salary;
+
+            }
+            else if (Salary > 5000 && Salary < 10000)
+            {
+                HRA = 0.15 * Salary;
+                TA = 0.10 * Salary;
+                DA = 0.2 * Salary;
+
+            }
+            else if (Salary > 10000 && Salary < 15000)
+            {
+                HRA = 0.2 * Salary;
+                TA = 0.15 * Salary;
+                DA = 0.25 * Salary;
+
+            }
+            else if (Salary > 15000 && Salary < 20000)
+            {
+                HRA = 0.25 * Salary;
+                TA = 0.20 * Salary;
+                DA = 0.30 * Salary;
+
+            }
+            else if (Salary <= 20000)
+            {
+                HRA = 0.3 * Salary;
+                TA = 0.25 * Salary;
+                DA = 0.35 * Salary;
+
+            }
+
+            GrossSalary = Salary + HRA + TA + DA;
+            return GrossSalary;
+        }
+
+        public double CalculateSalary()
+        {
+            double gross = CalculateGross();
+            PF = 0.10 * gross;
+            TDS = 0.18 * gross;
+            NetSalary = gross - (PF + TDS);
+            return NetSalary;
+        }
+        public void DisplayDetails()
+        {
+            double net = CalculateSalary();
+            Console.WriteLine($@"
+                                 Employee Id : {EmpNo}
+                                 Name : {EmployeeName} 
+                                 Employee Salary: {net}
+                               ");
+        }
+    }
+}
---
+++ Assignment 2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+
+            Console.WriteLine("Employee ID: ");
+            employee.EmpNo = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Employee Name: ");
+            employee.EmployeeName = Console.ReadLine();
+
+            Console.WriteLine("Salary");
+            employee.Salary = double.Parse(Console.ReadLine());
+
+            double grosssalary = employee.CalculateGross();
+            Console.WriteLine("Gross Salary :" + grosssalary);
+
+            double netsalary = employee.CalculateSalary();
+            Console.WriteLine("NetSalary :" + netsalary);
+
+
+            employee.DisplayDetails();
+        }
+    }
+}
